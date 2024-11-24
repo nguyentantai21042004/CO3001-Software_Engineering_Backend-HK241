c000b848d34f2c6e2882eff52ad5984685ce3189
@tai21042004 <tai21042004@gmail.com>
2024-11-24 11:28:18 +0700

    Fix Firebase Config

--- src/main/java/com/project/backend/configurations/FirebaseConfig.java
+++ src/main/java/com/project/backend/configurations/FirebaseConfig.java
@@ -3,22 +3,18 @@ package com.project.backend.configurations;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
-
-    @PostConstruct
-    public void init() {
+    public FirebaseConfig() {
         try {
-            // Đọc file từ classpath
             InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setStorageBucket("testbe-28a98.appspot.com")
                     .build();
--- src/main/java/com/project/backend/services/firebase/FirebaseStorageService.java
+++ src/main/java/com/project/backend/services/firebase/FirebaseStorageService.java
@@ -1,11 +1,10 @@
 package com.project.backend.services.firebase;
-import com.google.auth.oauth2.GoogleCredentials;
+
 import com.google.cloud.storage.*;
 import com.google.firebase.cloud.StorageClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -14,35 +13,21 @@ public class FirebaseStorageService {
 
     private final Storage storage;
 
-    public FirebaseStorageService() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials
-                .fromStream(new FileInputStream("src/main/resources/serviceAccountKey.json"));
-        this.storage = StorageOptions.newBuilder()
-                .setCredentials(credentials)
-                .build()
-                .getService();
+    public FirebaseStorageService() {
+        this.storage = StorageClient.getInstance().bucket().getStorage();
     }
 
     public String uploadFile(MultipartFile file) throws IOException {
-        // Tạo một tên file duy nhất
         String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
-
-        // Lấy bucket từ Firebase Storage
         Bucket bucket = StorageClient.getInstance().bucket();
-
-        // Upload file lên Firebase Storage
         Blob blob = bucket.create(fileName, file.getBytes(), file.getContentType());
-        blob.createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.OWNER));
-
-        // Trả về URL của file đã upload
+        blob.createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
         return blob.getMediaLink();
     }
 
     public boolean deleteFile(String filePath) {
         String bucketName = "testbe-28a98.appspot.com";
         Bucket bucket = storage.get(bucketName);
-
-        // Delete the file
         return bucket.get(filePath).delete();
     }
 }
