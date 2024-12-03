6a92151deb1471667cc2663f122836c9a8df4eb8
Andy <andybk.nguyen1202@gmail.com>
2024-12-03 16:08:04 +0700

    add report feature

---
+++ src/main/java/com/project/backend/controllers/ReportController.java
@@ -0,0 +1,41 @@
+package com.project.backend.controllers;
+
+import com.google.firebase.database.core.Repo;
+import com.project.backend.models.Report;
+import com.project.backend.responses.ResponseObject;
+import com.project.backend.responses.report.ReportResponse;
+import com.project.backend.services.report.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("${api.prefix}/reports")
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportService reportService;
+
+    @GetMapping("/{year}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('SPSO')")
+    public ResponseEntity<ResponseObject> getReport(@PathVariable Integer year) throws Exception{
+        List<Report> reportList = reportService.getReport(year);
+
+        List<ReportResponse> reportResponseList = reportList.stream()
+                .map(ReportResponse::fromReport)
+                .toList();
+
+        return ResponseEntity.ok().body(ResponseObject.builder()
+                .message("Get report successfully")
+                .status(HttpStatus.OK)
+                .data(reportResponseList)
+                .build());
+    }
+}
---
+++ src/main/java/com/project/backend/responses/report/ReportResponse.java
@@ -0,0 +1,68 @@
+package com.project.backend.responses.report;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.backend.models.Report;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReportResponse {
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("spso_id")
+    private Integer spsoId;
+
+    @JsonProperty("paper_purchase_count")
+    private Integer paperPurchaseCount;
+
+    @JsonProperty("paper_sold_count")
+    private Integer paperSoldCount;
+
+    @JsonProperty("revenue_prom_paper_sales")
+    private Integer revenueFromPaperSales;
+
+    @JsonProperty("printed_a4_pages")
+    private Integer printedA4Pages;
+
+    @JsonProperty("printed_a3_pages")
+    private Integer printedA3Pages;
+
+    @JsonProperty("print_job_count")
+    private Integer printJobCount;
+
+    @JsonProperty("start_date")
+    private LocalDate startDate;
+
+    @JsonProperty("end_date")
+    private LocalDate endDate;
+
+    @JsonProperty("type")
+    private String type;
+
+    public static ReportResponse fromReport(Report report){
+        if(report == null) return null;
+        return ReportResponse.builder()
+                .id(report.getId())
+                .spsoId(report.getSpsoId())
+                .paperPurchaseCount(report.getPaperPurchaseCount())
+                .paperSoldCount(report.getPaperSoldCount())
+                .revenueFromPaperSales(report.getRevenueFromPaperSales())
+                .printedA4Pages(report.getPrintedA4Pages())
+                .printedA3Pages(report.getPrintedA3Pages())
+                .printJobCount(report.getPrintJobCount())
+                .startDate(report.getStartDate())
+                .endDate(report.getEndDate())
+                .type(report.getType())
+                .build();
+    }
+}
